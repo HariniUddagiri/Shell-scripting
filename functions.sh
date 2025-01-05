e12f920a4656d410a3497f99c969a44d4145a502
HariniUddagiri <157795052+HariniUddagiri@users.noreply.github.com>
2025-01-05 15:45:26 +0000

    functions.sh

--- functions.sh
+++ functions.sh
@@ -2,9 +2,13 @@
 
 USER=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
 if [ $USER -ne 0 ]
 then
-echo "Root access is needed"
+echo "Error:$R Root access is needed"
 exit 1
 fi
 
@@ -12,26 +16,26 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
 dnf install mysql -y
-repeat $? "Installing mysql"
+repeat $? "$Y Installing mysql"
 else
-echo "Mysql already installed"
+echo "$G Mysql already installed"
 fi
 
 dnf list installed git
 if [ $? -ne 0 ]
 then
 dnf install git -y
-repeat $? "Installing git"
+repeat $? "$Y Installing git"
 else
-echo "git already installed"
+echo "$G git already installed"
 fi
 
 repeat() {
     if [ $1 -ne 0 ]
 then
-echo "$2..failure"
+echo "$2..$R failure"
 exit 1
 else
-echo "$2..success"
+echo "$2..$G success"
 fi
 }
