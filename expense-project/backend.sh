a22b4e01aaf138ae5f211472e796eca9b6c3f047
HariniUddagiri <157795052+HariniUddagiri@users.noreply.github.com>
2025-01-06 13:23:23 +0000

    backend

--- expense-project/backend.sh
+++ expense-project/backend.sh
@@ -43,29 +43,47 @@ echo "Script started executing at $timestamp" &>>$Logfilename
 
 Check $USER
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$Logfilename
 Repeat $? "Disabling nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$Logfilename
 Repeat $? "Enabling nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$Logfilename
 Repeat $? "Installing nodejs"
 
 useradd expense
 
-mkdir -p /app
+mkdir -p /app &>>$Logfilename
 Repeat $? "Creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$Logfilename
 Repeat $? "Downloading backend code"
 
 cd /app
 
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>$Logfilename
 Repeat $? "unzipping the code"
 
-npm install
+npm install &>>$Logfilename
 Repeat $? "Installing dependencies"
 
-cp  /etc/systemd/system/backend.service
+cp /Shell-scripting/expense-project/backend.service /etc/systemd/system/backend.service &>>$Logfilename
+
+systemctl daemon-reload &>>$Logfilename
+Repeat $? "Reload daemon"
+
+systemctl start backend &>>$Logfilename
+Repeat $? "starting the backend"
+
+systemctl enable backend &>>$Logfilename
+Repeat $? "Enabling the backend"
+
+dnf install mysql -y &>>$Logfilename
+Repeat $? "Installing Mysql"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$Logfilename
+Repeat $? "Setting up the transactions schema and tables"
+
+systemctl restart backend &>>$Logfilename
+Repeat $? "Restarting backend"
