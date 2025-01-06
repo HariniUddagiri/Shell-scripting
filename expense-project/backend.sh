ec3fd7c076a32a785a7318a1ceb26c83e81d3454
HariniUddagiri <157795052+HariniUddagiri@users.noreply.github.com>
2025-01-06 13:54:21 +0000

    backend

--- expense-project/backend.sh
+++ expense-project/backend.sh
@@ -61,6 +61,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 Repeat $? "Downloading backend code"
 
 cd /app
+rm -rf /app/*
 
 
 unzip /tmp/backend.zip &>>$Logfilename
