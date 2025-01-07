34ce7c1e509266a05ab503f57d1634efa3e8e498
HariniUddagiri <157795052+HariniUddagiri@users.noreply.github.com>
2025-01-07 17:59:20 +0000

    samp

--- samp.sh
+++ samp.sh
@@ -9,8 +9,8 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
 
-LOGS_FOLDER="/home/ec2-user/shellscript-logs"
-LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
+LOGS_FOLDER="var/log/Shell-scripting"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
@@ -20,7 +20,7 @@ USAGE(){
     exit 1
 }
 
-mkdir -p /home/ec2-user/shellscript-logs
+mkdir -p var/log/Shell-scripting
 
 if [ $# -lt 2 ]
 then
