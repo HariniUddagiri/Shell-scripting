f28a1b944a9c623064b43c86053ef321840407c6
HariniUddagiri <157795052+HariniUddagiri@users.noreply.github.com>
2025-01-07 20:53:27 +0000

    disk_usage

---
+++ disk_usage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Disk_space=$(df -hT | grep 'xfs')
+Disk_threshold=5
+
+while read -r line
+do
+partition=$(echo $line | awk -F " " '{print $NF}')
+Usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+if [ $Usage -ge $Disk_threshold ]
+then
+msg="High disk usage on partition:$partition, Usage:$Usage"
+fi
+done >>> $Disk_space
+
+echo $msg
+
+echo "High disk usage alert $(date)" | mutt -s "msg" hunny11147@gmail.com
